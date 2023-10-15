e4e8956b3016ee4a97ebf2692f1e1292f3176d82
DOUHIChaimae <c.douhi@etu.enset-media.ac.ma>
2023-10-15 14:56:27 +0100

    refactoring

--- gateway/src/main/java/ma/enset/gateway/GatewayApplication.java
+++ gateway/src/main/java/ma/enset/gateway/GatewayApplication.java
@@ -17,8 +17,9 @@ public class GatewayApplication {
     @Bean
     RouteLocator routeLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("r1", r -> r.path("/customers/**").uri("http://localhost:8085/"))
-                .route("r2", r -> r.path("/products/**").uri("http://localhost:8086/"))
+//                .route("r1", r -> r.path("/customers/**").uri("http://localhost:8085/"))
+                .route("r1", r -> r.path("/customers/**").uri("lb://CUSTOMER-SERVICE"))
+                .route("r2", r -> r.path("/products/**").uri("lb://PRODUCT-SERVICE"))
                 .build();
     }
 }
