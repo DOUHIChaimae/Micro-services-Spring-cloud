04f28cbdb2d1f59fe5f1f824d394a7a5fcf16e73
DOUHIChaimae <c.douhi@etu.enset-media.ac.ma>
2023-10-15 11:10:53 +0100

    add some products

--- InventorySevice/src/main/java/ma/enset/inventorysevice/InventorySeviceApplication.java
+++ InventorySevice/src/main/java/ma/enset/inventorysevice/InventorySeviceApplication.java
@@ -1,13 +1,29 @@
 package ma.enset.inventorysevice;
 
+import ma.enset.inventorysevice.entities.Product;
+import ma.enset.inventorysevice.repositories.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventorySeviceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(InventorySeviceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner start(ProductRepository productRepository) {
+        return args -> {
+            productRepository.save(new Product(null, "PC", 12000, 17));
+            productRepository.save(new Product(null, "Imprimante", 15000, 2));
+            productRepository.save(new Product(null, "Smartphone", 1000, 5));
+            productRepository.findAll().forEach(product -> {
+                System.out.println(product.getName());
+            });
+        };
     }
 
 }
