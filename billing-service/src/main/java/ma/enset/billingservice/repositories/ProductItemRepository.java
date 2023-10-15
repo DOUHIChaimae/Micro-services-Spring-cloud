3c3bc419fc3eca8edb324a5b841b1bd57f9099fb
DOUHIChaimae <c.douhi@etu.enset-media.ac.ma>
2023-10-15 23:36:37 +0100

    findById()

--- billing-service/src/main/java/ma/enset/billingservice/repositories/ProductItemRepository.java
+++ billing-service/src/main/java/ma/enset/billingservice/repositories/ProductItemRepository.java
@@ -3,5 +3,8 @@ package ma.enset.billingservice.repositories;
 import ma.enset.billingservice.entities.ProductItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+
 public interface ProductItemRepository extends JpaRepository<ProductItem, Long> {
+    public Collection<ProductItem> findById(long id);
 }
