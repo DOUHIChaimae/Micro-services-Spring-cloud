c14225a8f6810b7e6531a25a49a2d9bd9f55fa94
DOUHIChaimae <c.douhi@etu.enset-media.ac.ma>
2023-10-15 15:34:00 +0100

    dynamic conf

--- gateway/src/main/java/ma/enset/gateway/GatewayApplication.java
+++ gateway/src/main/java/ma/enset/gateway/GatewayApplication.java
@@ -2,6 +2,9 @@ package ma.enset.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -13,13 +16,23 @@ public class GatewayApplication {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
-    //Conf JAVA
-    @Bean
+    //Conf JAVA ==> static conf
+    //@Bean
     RouteLocator routeLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-//                .route("r1", r -> r.path("/customers/**").uri("http://localhost:8085/"))
+                //.route("r1", r -> r.path("/customers/**").uri("http://localhost:8085/"))
                 .route("r1", r -> r.path("/customers/**").uri("lb://CUSTOMER-SERVICE"))
                 .route("r2", r -> r.path("/products/**").uri("lb://PRODUCT-SERVICE"))
                 .build();
     }
+
+
+    //dynamic conf
+    @Bean
+    DiscoveryClientRouteDefinitionLocator definitionLocator(
+            ReactiveDiscoveryClient discoveryClient,
+            DiscoveryLocatorProperties properties) {
+        return new DiscoveryClientRouteDefinitionLocator(discoveryClient, properties);
+    }
+
 }
