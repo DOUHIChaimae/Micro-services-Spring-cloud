6d2529b8242b903cef0190ea4d334a19f32fec6d
DOUHIChaimae <c.douhi@etu.enset-media.ac.ma>
2023-10-23 11:04:23 +0100

    exposeIdsFor Product and Customer class

--- billing-service/src/main/java/ma/enset/billingservice/BillingServiceApplication.java
+++ billing-service/src/main/java/ma/enset/billingservice/BillingServiceApplication.java
@@ -1,13 +1,39 @@
 package ma.enset.billingservice;
 
+import ma.enset.billingservice.feign.CustomerRestClient;
+import ma.enset.billingservice.feign.ProductItemRestClient;
+import ma.enset.billingservice.models.Customer;
+import ma.enset.billingservice.repositories.BillRepository;
+import ma.enset.billingservice.repositories.ProductItemRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Collection;
 
 @SpringBootApplication
+@EnableFeignClients
 public class BillingServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BillingServiceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner start(BillRepository billRepository,
+                            ProductItemRepository productItemRepository,
+                            CustomerRestClient customerRestClient,
+                            ProductItemRestClient productItemRestClient
+    ) {
+        return args -> {
+            Customer customer = customerRestClient.getCustomerById(1L);
+            System.out.println("------------------------");
+            System.out.println(customer.getId());
+            System.out.println(customer.getName());
+            System.out.println(customer.getEmail());
+        };
     }
 
 }
--- billing-service/src/main/java/ma/enset/billingservice/feign/ProductItemRestClient.java
+++ billing-service/src/main/java/ma/enset/billingservice/feign/ProductItemRestClient.java
@@ -1,12 +1,17 @@
 package ma.enset.billingservice.feign;
 
-import ma.enset.billingservice.models.Customer;
+import ma.enset.billingservice.models.Product;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "CUSTOMER-SERVICE")
-public interface CustomerRestClient {
-    @GetMapping(path = "cutomers/{id}")
-    public Customer getCustomerById(@PathVariable(name = "id") Long id);
+@FeignClient(name = "PRODUCT-SERVICE")
+public interface ProductItemRestClient {
+    @GetMapping(path = "/products")
+    PagedModel<Product> pageProducts(@RequestParam(name = "page") int page, @RequestParam(name = "size") int size);
+
+    @GetMapping(path = "/products/{id}")
+    Product getProductById(@PathVariable Long id);
 }
